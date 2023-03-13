bd30892e9b7597da07d1175e2e674e125f67f6fb
Sachin Mahajan <114292297+sachin72m@users.noreply.github.com>
2023-03-13 18:45:57 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
 //your JS code here. If required.
+function display() {
+	var x = document.getElementById('fname').value;
+	var y = document.getElementById('lname').value;
+	var z = document.getElementById('phone').value;
+	var p = document.getElementById('email').value;
+
+	var X = "First Name:";
+	var Y = "Last Name:";
+	var Z = "Phone Number:";
+	var P = "Email ID:";
+	
+alert(X+x  +"\n"+ Y+y  +"\n"+  Z+z   +"\n"+   P+p);
+}
